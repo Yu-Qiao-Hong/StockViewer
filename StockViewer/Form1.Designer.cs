41c6b133ebfb94158b1d1444d747d9db0c3ff38b
Iverson127 <Iverson.Hong@gmail.com>
2016-05-15 22:16:30 +0800

    Update

---
+++ StockViewer/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace StockViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Column_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1_UpDown = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(3, 234);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(278, 24);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.trackBar1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.menuStrip1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.settingToolStripMenuItem.Text = "Option";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column_Name,
+            this.Column_Price,
+            this.Column1_UpDown});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(278, 195);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Column_Name
+            // 
+            this.Column_Name.Frozen = true;
+            this.Column_Name.HeaderText = "Name";
+            this.Column_Name.Name = "Column_Name";
+            this.Column_Name.ReadOnly = true;
+            this.Column_Name.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column_Name.Width = 75;
+            // 
+            // Column_Price
+            // 
+            this.Column_Price.HeaderText = "Price";
+            this.Column_Price.Name = "Column_Price";
+            this.Column_Price.ReadOnly = true;
+            this.Column_Price.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column_Price.Width = 75;
+            // 
+            // Column1_UpDown
+            // 
+            this.Column1_UpDown.HeaderText = "+/-";
+            this.Column1_UpDown.Name = "Column1_UpDown";
+            this.Column1_UpDown.ReadOnly = true;
+            this.Column1_UpDown.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1_UpDown.Width = 75;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "StockViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1_UpDown;
+
+    }
+}
+
---
+++ StockViewer/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockViewer
+{
+    public partial class Form1 : Form
+    {
+        List<int> stockIdList = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stockIdList = new List<int>();
+
+            stockIdList.Add(2317);
+            stockIdList.Add(1101);
+            stockIdList.Add(2308);
+        }
+
+        private void Start()
+        {
+            Debug.WriteLine("Start start");
+            for (int i = 0; i < stockIdList.Count; i++)
+            {
+                GetStockInformation(i, stockIdList[i].ToString());
+            }
+            Thread.Sleep(5);
+            Debug.WriteLine("Start finish");
+        }
+
+        private bool GetStockInformation(int rowIdx, string stockId)
+        {
+            string url = @"https://tw.stock.yahoo.com/q/q?s=" + stockId;
+
+            WebClient web = new WebClient();
+            string str = web.DownloadString(url);
+
+            string startStr = "<td align=\"center\" bgcolor=\"#FFFfff\" nowrap>";
+            //string endStr = "<td align=center width=137 class=\"tt\">";
+            string endStr = "<tr bgcolor=\"#fdedef\">";
+
+            int startPos = str.IndexOf(startStr);
+            int endPos = str.IndexOf(endStr);
+
+            if (startPos == -1 || endPos == -1)
+            {
+                InsertData(rowIdx, new Stock());
+                return false;
+            }
+
+            string realStr = str.Substring(startPos, endPos - startPos);
+
+            Stock stock = ParseStockData(stockId, realStr);
+            InsertData(rowIdx, stock);
+
+
+            return true;
+        }
+
+        private void InsertData(int rowIdx, Stock stock)
+        {
+            DataGridViewRowCollection rows = dataGridView1.Rows;
+            var row = rows[rowIdx];
+            if (row.IsNewRow)
+                rows.Add(new object[] { stock.Name, stock.DealPrice, stock.UpDown });
+            else
+            {
+                rows.RemoveAt(rowIdx);
+                rows.Insert(rowIdx, new object[] { stock.Name, stock.DealPrice, stock.UpDown });
+            }
+        }
+
+        private Stock ParseStockData(string stockId, string realStr)
+        {
+            Stock stock = new Stock();
+            stock.ID = stockId;
+
+            Match match = Regex.Match(realStr, "<td align=\"center\" bgcolor=\"#FFFfff\" nowrap>(<b>)?([0-9.,:]+)(</b>)?</td>");
+            stock.Time = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.DealPrice = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.BuyPrice = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.SellPrice = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.TotalCount = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.Yesterday = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.Opening = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.High = match.Groups[2].ToString();
+
+            match = match.NextMatch();
+            stock.Low = match.Groups[2].ToString();
+
+            //<td align="center" bgcolor="#FFFfff" nowrap><font color=#ff0000>△1.1
+            //<td align="center" bgcolor="#FFFfff" nowrap><font color=#009900>▽0.70
+            match = Regex.Match(realStr, "<td align=\"center\" bgcolor=\"#FFFfff\" nowrap><font color=#(ff0000)?(009900)?>(.+)");
+            stock.UpDown = match.Groups[3].ToString();
+
+            //<input type=hidden name="stkname" value="鴻海">
+            match = Regex.Match(realStr, "<input type=hidden name=\"stkname\" value=\"(.+)\">");
+            stock.Name = match.Groups[1].ToString();
+
+            return stock;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = (double)trackBar1.Value / trackBar1.Maximum;
+        }
+
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (stopToolStripMenuItem.Text == "Stop")
+            {
+                Debug.WriteLine("Run");
+                stopToolStripMenuItem.Text = "Run";
+                timer1.Start();
+            }
+            else
+            {
+                Debug.WriteLine("Stop");
+                stopToolStripMenuItem.Text = "Stop";
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start();
+        }
+    }
+
+    class Stock
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+        public string DealPrice { get; set; }
+        public string BuyPrice { get; set; }
+        public string SellPrice { get; set; }
+        public string UpDown { get; set; }
+        public string TotalCount { get; set; }
+        public string Yesterday { get; set; }
+        public string Opening { get; set; }
+        public string High { get; set; }
+        public string Low { get; set; }
+    }
+}
